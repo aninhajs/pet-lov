02d097c280f50846883fcf9701cafe4eec786c18
Letícia Martins <lesilvaetrab008@gmail.com>
2025-12-05 23:35:16 -0300

    Feat: new screen candidate's detail

--- Abrece/src/pages/Questionnaire.jsx
+++ Abrece/src/pages/Questionnaire.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import QuestionnaireService from "../services/QuestionnaireService";
 
-const sections = [
+export const sections = [
   {
     id: "adotante",
     title: "Informações do adotante",
